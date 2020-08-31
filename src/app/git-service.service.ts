f39e15341075f59e3dcf2d953de537282c9dc33c
DerrickOdhiambo <odhiamboderrick56@gmail.com>
2020-08-31 13:23:52 +0300

    removed the function for getting user repo to move it to a different service folder

--- src/app/git-service.service.ts
+++ src/app/git-service.service.ts
@@ -8,11 +8,7 @@ import { environment } from '../environments/environment';
 })
 export class GitServiceService {
   constructor(private http: HttpClient) {}
-  getUserData(): Observable<any> {
-    return this.http.get(
-      `https://api.github.com/users?token=${environment.apiKey}`
-    );
-  }
+
   searchUser(username): Observable<any> {
     return this.http.get(
       `https://api.github.com/users/${username}?token=${environment.apiKey}`
