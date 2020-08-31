f7dfdf05ffac6f919374fc2295b95982e9536a86
DerrickOdhiambo <odhiamboderrick56@gmail.com>
2020-08-31 13:24:51 +0300

    create a service to make a http request for getting user repositories

---
+++ src/app/repo.service.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../environments/environment';
+import { Repo } from './classes/repo';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class RepoService {
+  repositories: Repo;
+  constructor(private http: HttpClient) {}
+
+  // repoRequest() {
+  //   interface ApiResponse {}
+  //   let promise = new Promise((resolve, reject) => {
+  //     this.http
+  //       .get<ApiResponse>(
+  //         `https://api.github.com/users/token=${environment.apiKey}`
+  //       )
+  //       .toPromise()
+  //       .then(
+  //         (response) => {
+  //           console.log(response);
+
+  //           resolve();
+  //         },
+  //         (error) => {
+  //           reject(error);
+  //         }
+  //       );
+  //   });
+  //   return promise;
+  // }
+
+  getUserData(userName: string): Observable<any> {
+    return this.http.get(
+      `https://api.github.com/users/${userName}/repos?token=${environment.apiKey}`
+    );
+  }
+}
