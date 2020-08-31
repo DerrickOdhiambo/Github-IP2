1d2b8237f1a6d0565afe6c0c284dbbf6c711b51b
DerrickOdhiambo <odhiamboderrick56@gmail.com>
2020-08-31 06:32:58 +0300

    add routing paths to the module and also a redirect path is path is empty

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DisplayComponent } from './display/display.component';
+import { RepoDetailsComponent } from './repo-details/repo-details.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'main', component: DisplayComponent },
+  { path: 'repo', component: RepoDetailsComponent },
+  { path: '', redirectTo: '/main', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
+export const routingComponents = [DisplayComponent, RepoDetailsComponent];
