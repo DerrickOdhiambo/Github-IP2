12f305a18065f3899ab4b084a0cc00d3ef0adb0f
DerrickOdhiambo <odhiamboderrick56@gmail.com>
2020-09-01 11:34:49 +0300

    add a progress bar to the website

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
+import { NgProgressModule } from '@ngx-progressbar/core';
+import { NgProgressHttpClientModule } from '@ngx-progressbar/http-client';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -16,7 +18,14 @@ import { routingComponents } from './app-routing.module';
     FooterComponent,
     routingComponents,
   ],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule, FormsModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    NgProgressModule.forRoot(),
+    NgProgressHttpClientModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
